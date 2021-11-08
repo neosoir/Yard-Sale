6ddb7adc6859f01d701c239f5a51d7c054a8af82
Neo Soir <gipsypas@neosoir.com>
2021-11-08 17:34:37 +0100

    adaptamos los hooks de storefront a nuestro tema personalizado

--- footer.php
+++ footer.php
@@ -1,9 +1,15 @@
-</div>
-    </main>
+</div><!-- .col-full -->
+	</div><!-- #content -->
+
+    <?php do_action( 'storefront_before_footer' ); ?>
+
 
     <footer class="text-center my-4">
         <?php dynamic_sidebar('pie-pagina') ?>
     </footer>
+    
+    <?php do_action( 'storefront_after_footer' ); ?>
+
 <script src="assets/js/script.js"></script>
 
 <?php wp_footer(); ?>
